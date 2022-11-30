6b8564f3c545430b2934f02a64d7ee3fcba0b31f
isaiassiele <116893742+isaiassiele@users.noreply.github.com>
2022-11-30 16:05:43 +0000

    Modifying variable connections

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const app = express ();
+const app = express();
 
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 
@@ -14,7 +14,7 @@ app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://issume:K3Ayh2vR34Jv62Yj@issume.q7r6fty.mongodb.net/?retryWrites=true&w=majority';
 
-const PORT  = process.env.PORT || 5000 ;
+const PORT  = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser:true, useUnifiedTopology:true
@@ -22,4 +22,4 @@ mongoose.connect(CONNECTION_URL, {
     console.log('Connection is established and running on port:${PORT}')
 )).catch((err)=>console.log(err.message));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify',false);
