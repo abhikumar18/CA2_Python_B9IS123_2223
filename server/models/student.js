415baec04bb9b27885ab19a0d1b8ec00566c919d
isaiassiele <116893742+isaiassiele@users.noreply.github.com>
2022-12-02 23:26:59 +0000

    Creating a function that stores an object

---
+++ server/models/student.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const studentSchema = mongoose.Schema({
+    resgistrationNumber: Number,
+    name:String,
+    grade: String,
+    section: {
+        type: String,
+        default: 'A'
+    },
+    subjects:[String]
+});
+
+const student = mongoose.model('student', studentSchema);
+
+export default student;
