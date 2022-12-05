bd8a91e06e07890573341c15c083c27c7a47a5f0
isaiassiele <116893742+isaiassiele@users.noreply.github.com>
2022-12-05 23:35:39 +0000

    Modifying Some error Importing User Interface Librarires

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Student Information</h1>
-      </header>
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit" >
+          <Typography className={classes.heading} variant="h2" align="center">Students Create and Show</Typography>
+        </AppBar>
+
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="strect">
+              <Grid item xs={12} sm={7}>
+
+              </Grid>
+              <Grid item xs={12} sm={4}>
+
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+      
     </div>
   );
 }
