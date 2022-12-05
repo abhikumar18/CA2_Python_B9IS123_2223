992e8c312ea0d54c1bf95ab15af5db9cca11f7a8
isaiassiele <116893742+isaiassiele@users.noreply.github.com>
2022-12-05 23:53:13 +0000

    Creating Components for students

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Student from './components/showStudent/showStudent.js';
+import Create from './components/createStudent/createStudent.js'
 import './App.css';
 
 function App() {
@@ -14,10 +16,16 @@ function App() {
           <Container>
             <Grid container justify="space-between" alignItems="strect">
               <Grid item xs={12} sm={7}>
-
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                   <Student />
+                </AppBar>
               </Grid>
               <Grid item xs={12} sm={4}>
-
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                   < Create />
+                </AppBar>
+                
+                
               </Grid>
             </Grid>
           </Container>
