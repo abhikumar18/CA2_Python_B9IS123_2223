2dac0ae4c49e1ca50ac8f4a964e18117edf6a8d6
isaiassiele <116893742+isaiassiele@users.noreply.github.com>
2022-12-11 04:34:13 +0000

    Modifying the package-lock.json

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import studentRoutes from './routes/student.js';
 
-app.use('/students', studentRoutes);
+//app.use('/students', studentRoutes);
 
 const app = express();
 
@@ -15,14 +15,14 @@ app.use(bodyParser.urlencoded({limit:"20mb", extended:true}));
 app.use(cors());
 
 
-const CONNECTION_URL = 'mongodb+srv://issume:K3Ayh2vR34Jv62Yj@issume.q7r6fty.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://issume:1234@cluster0.02zx9mt.mongodb.net/?retryWrites=true&w=majority';
 
-const PORT  = process.env.PORT || 5500;
+const PORT  = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser:true, useUnifiedTopology:true
 }).then(()=>app.listen(PORT, ()=>
-    console.log('Connection is established and running on port:${PORT}')
+    console.log(`Connection is established and running on port: ${PORT}`)
 )).catch((err)=>console.log(err.message));
 
-mongoose.set('useFindAndModify',false);
+//mongoose.set('useFindAndModify',false);
