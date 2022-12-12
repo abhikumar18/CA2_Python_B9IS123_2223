7f27a2d62143b79359243d0cf448378fc5db08a1
abhikumar18 <abhishek.it.msit@gmail.com>
2022-12-13 04:44:32 +0530

    Delete functionality at server

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,5 +1,6 @@
 import StudentData from '../models/student.js'
 
+// Get Students Data
 export const getStudents = async (req,res) =>{
     try{
         const allStudents = await StudentData.find();
@@ -10,6 +11,8 @@ export const getStudents = async (req,res) =>{
     }
 }
 
+
+// Creating students Data
 export const createStudent = async(req,res)=>{
     const student = req.body;
 
@@ -19,5 +22,16 @@ export const createStudent = async(req,res)=>{
         res.status(201).json(newStudent);
     }catch(error){
         res.status(409).json({message:error.message});
+    }
+}
+
+// Deleting Student Data
+export const deleteStudent = async(req,res)=>{
+    const id = req.params.id;
+    try{
+        await (await StudentData.findByIdAndRemove(id)).exec();
+        res.send("Succesfully Deleted")
+    }catch(error){
+        console.log(error);
     }
 }
