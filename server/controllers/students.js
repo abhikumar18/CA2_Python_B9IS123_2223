88c40cfd21e51d1c3775086f327672237f47c5c0
isaiassiele <116893742+isaiassiele@users.noreply.github.com>
2022-12-15 02:34:12 +0000

    Importing students data from routes

--- server/controllers/students.js
+++ server/controllers/students.js
@@ -1,6 +1,8 @@
+import StudentData from '../models/student.js';
+
 export const getStudents =  async (req, res)=> {
     try {
-        const allStudents = await student.find();
+        const allStudents = await StudentData.find();
 
         res.status(200).json(allStudents);
     } catch (error) {
@@ -11,7 +13,7 @@ export const getStudents =  async (req, res)=> {
 export const createStudent = async (req, res)=> {
     const student = req.body;
 
-    const newStudent = new student(student);
+    const newStudent = new StudentData(student);
 
     try {
        await newStudent.save();
--- server/routes/student.js
+++ server/routes/student.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { getStudents, createStudent } from '../controllers/students.js';
 import student from '../models/student.js';
+
 const router = express.Router();
 
 router.get('/', getStudents);
