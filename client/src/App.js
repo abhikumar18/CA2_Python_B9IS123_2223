84f47027092171c9e000d6f29b95a4fa72e6e0b2
sohaib_khan11 <sohaibkhan.ninesol@gmail.com>
2022-12-15 21:10:48 +0000

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 
 function App() {
+
+  const classes = useStyles();
+
   return (
     <div className="App">
-      
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">Student Management System</Typography>
+        </AppBar>
+
+        <Grow in>
+            <Container>
+              <Grid container justify="space-between" alignItems='stretch'>
+                <Grid item xs={12} sm={7}>
+                  <Student/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                   <AppBar className={classes.appBar} position="static" color='inherit'>
+                    <Create/>
+                   </AppBar>
+                </Grid>
+
+              </Grid>
+            </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
