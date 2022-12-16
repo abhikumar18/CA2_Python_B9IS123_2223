5875b809d5f5316de472557d126b7c3dd487e48a
isaiassiele <116893742+isaiassiele@users.noreply.github.com>
2022-12-16 04:32:36 +0000

    Cleaning Errors fro Create Function

--- server/controllers/students.js
+++ server/controllers/students.js
@@ -10,16 +10,28 @@ export const getStudents =  async (req, res)=> {
     }
 }
 
-export const createStudent = async (req, res)=> {
+
+export const createStudent = async(req,res)=>{
     const student = req.body;
 
     const newStudent = new StudentData(student);
+    try{
+        await newStudent.save();
+        res.status(201).json(newStudent);
+    }catch(error){
+        res.status(409).json({message:error.message});
+    }
+}
 
+export const deleteStudent = async (req, res)=> {
+    const id = req.params.id;
     try {
-       await newStudent.save();
-       res.status(201).json(newStudent);
+       await StudentData.findByIdAndRemove(id).exec();
+       res.send('Successfully Deleted!')
     } catch (error) {
-        res.status(409).json({message: error.message});
+        console.log(error);
         
     }
 }
+
+
